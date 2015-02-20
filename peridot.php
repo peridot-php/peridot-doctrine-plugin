edca30bd3abe927e0b3658129bf30a59914d5922
Kristopher Wilson <kristopherwilson@gmail.com>
2015-02-20 11:37:06 -0500

    composer + peridot suite setup

---
+++ peridot.php
@@ -0,0 +1,11 @@
+<?php
+
+use Evenement\EventEmitterInterface;
+use Peridot\Console\Environment;
+
+return function (EventEmitterInterface $emitter) {
+    $emitter->on('peridot.start', function (Environment $env) {
+        $definition = $env->getDefinition();
+        $definition->getArgument('path')->setDefault('specs');
+    });
+};
